16eb882228286c9a274b0a9002e9d3832dc8da58
Rasika Maduranga <rasikat77@gmail.com>
2024-05-20 23:08:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import requests
 import re
 
+type = [{'make':"suzuki", 'series':"alto"}, {'make':"honda", 'series':"civic"}, {'make':"suzuki", 'series':"celerio"}]
 # URL to fetch the data from
-url = "https://punchicar.com/inventory-2/?make=suzuki&serie=alto&ajax_action=listings-result"
 
-# Fetch the content from the URL
-response = requests.get(url)
-response.raise_for_status()  # Raise an exception for HTTP errors
+for i in type:
+    url = f"https://punchicar.com/inventory-2/?make={i['make']}&serie={i['series']}&ajax_action=listings-result"
 
-# Parse the JSON response
-data = response.json()
-match = re.search(r'Rs. ([0-9 ]+)',str(data))
-if match:
-    # price and replace Rs. with empty string
-    price = match.group(0).replace("Rs. ", "").replace(" ", "")
-    print(price) 
+    # Fetch the content from the URL
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    # Parse the JSON response
+    data = response.json()
+    match = re.search(r'Rs. ([0-9 ]+)',str(data))
+    if match:
+        # price and replace Rs. with empty string
+        price = match.group(0).replace("Rs. ", "").replace(" ", "")
+        print(i['series'] + ' , ' + price)
+
